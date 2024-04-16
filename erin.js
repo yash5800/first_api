646426a199e9ffa0ac3efc5dad522e6a1f07fe9f
A.Yaswanth Kiran <yaswanthjk580@gmail.com>
2024-04-16 15:18:33 +0530

    changes

--- erin.js
+++ erin.js
@@ -18,7 +18,7 @@ db.run('create table if not exists moko(key text primary key,value text)');
 
 
 app.get('/make_wet/:make/:wet', (req, res) => {
-    console.log("entered into auto saver");
+    console.log("entered into saver");
     const key = decodeURIComponent(req.params.make);
     const val = decodeURIComponent(req.params.wet);
     console.log(key);
@@ -73,79 +73,3 @@ app.get('/fuck/:id', (req, res) => {
 
 
 app.listen(port, () => console.log(`Server running on ${port}`));
-
-// const express = require('express');
-// const cors = require('cors');
-// const sqlite3 = require('sqlite3').verbose();
-
-// const app = express();
-
-// const port = process.env.PORT || 8080;
-
-// let db = new sqlite3.Database('./wets.db', err => {
-//     if (err) {
-//         console.log("No such file");
-//     }
-// });
-
-// app.use(cors());
-
-// db.run('CREATE TABLE IF NOT EXISTS moko(key TEXT PRIMARY KEY, value TEXT)');
-
-// db.get(`select * from moko`,(err,row)=>{
-//     if(err){
-//         console.log("error");
-//     }
-//     else{
-//         console.log(row);
-//     }
-// })
-
-// app.get('/make_wet/:make/:wet', (req, res) => {
-//     console.log("Entered into auto saver");
-//     const key = decodeURIComponent(req.params.make);
-//     const val = decodeURIComponent(req.params.wet);
-//     console.log(key)
-//     console.log(val)
-//     db.run(`UPDATE moko SET value = ? WHERE key = ?`, [val, key], err => {
-//         if (err) {
-//             console.log("Not updated");
-//         } else {
-
-//             console.log("Updated in database!!");
-//             res.json({ key: "done" });
-//         }
-//     });
-// });
-
-// app.get('/fuck/:id', (req, res) => {
-//     console.log("Entered");
-//     const key = req.params.id;
-//     console.log(key);
-//     // Check if key exists in database
-//     db.get(`SELECT * FROM moko WHERE key = ?`, [key], (err, row) => {
-//         if (err) {
-//             console.log("Something wrong");
-//             res.json({ key: "" });
-//         } else {
-//             if (row) {
-//                 console.log("Data: ", row);
-//                 res.json({ key: row.value });
-//             } else {
-//                 // If key doesn't exist in database, insert it with empty value
-//                 db.run(`INSERT INTO moko VALUES(?, '')`, [key], err => {
-//                     if (err) {
-//                         console.log("Not inserted");
-//                         res.json({ key: "" });
-//                     } else {
-//                         console.log("Inserted new key into database");
-//                         res.json({ key: "" });
-//                     }
-//                 });
-//             }
-//         }
-//     });
-// });
-
-// app.listen(port, () => console.log(`Server running on ${port}`));
-
